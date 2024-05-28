9a5aa852d6bacc51605da0ec281999ce6b059118
Akarsh8933 <akarshkatiyarak123@gmail.com>
2024-05-28 15:33:32 +0530

    Updated the file with new changes

--- disjoint_set_union.cpp
+++ disjoint_set_union.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 class Disjointset{
-    vector<int>rank,parent;
+    vector<int>rank,parent,size;
     public:
     Disjointset(int n){
         rank.resize(n+1,0);
+        size.resize(n+1,0);
         parent.resize(n+1);
         for(int i=0; i<=n; i++)parent[i]=i;
     }
@@ -30,14 +31,28 @@ class Disjointset{
             rank[ulp_u]++;
         }
     }
+
+    void unionBySize(int u, int v){
+        int ulp_u = findUltimateParent(u);
+        int ulp_v = findUltimateParent(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
 };
  
 int main(){
     Disjointset ds(7);
-    ds.unionByRank(1,2);
-    ds.unionByRank(2,3);
-    ds.unionByRank(4,5);
-    ds.unionByRank(5,6);
+    ds.unionBySize(1,2);
+    ds.unionBySize(2,3);
+    ds.unionBySize(4,5);
+    ds.unionBySize(5,6);
 
     //if 3 and 7 belong to smae component
     int x = ds.findUltimateParent(3);
@@ -48,7 +63,7 @@ int main(){
     else{
         cout<<"no"<<endl;
     }
-    ds.unionByRank(3,7);
+    ds.unionBySize(3,7);
 
     x = ds.findUltimateParent(3);
     y = ds.findUltimateParent(7);
